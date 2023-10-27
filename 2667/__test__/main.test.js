14d6125bb372e9bfdf0b8ebe6e8060fd196a27ac
BrianPritzel <148987359+BrianPritzel@users.noreply.github.com>
2023-10-27 09:36:48 -0500

    Added leetcode problem 2667

---
+++ 2667/__test__/main.test.js
@@ -0,0 +1,25 @@
+import { expect, test, vi } from "vitest"
+import { createHelloWorld } from "../src/main.js"
+
+
+test("example 1", () => {
+    //arrange
+    const args = [];
+    
+    //act
+    const f = createHelloWorld();
+
+    //assert
+    expect(f(...args)).toBe("Hello World");
+})
+
+test("example 2", () => {
+    //arrange
+    const args = [{},null,42];
+    
+    //act
+    const f = createHelloWorld();
+
+    //assert
+    expect(f(...args)).toBe("Hello World");
+})
---
+++ 2667/src/main.js
@@ -0,0 +1,13 @@
+export function createHelloWorld() {
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
