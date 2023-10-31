f711d644f0f8450729e6e3fbb87461c45081350a
BrianPritzel <148987359+BrianPritzel@users.noreply.github.com>
2023-10-31 09:14:20 -0500

    Added leetcode problem 2623

---
+++ 2623/src/main.js
@@ -0,0 +1,10 @@
+export function memoize(fn) {
+    const cache = {}
+    return function(...args) {
+        const key = JSON.stringify(args)
+        if (cache[key] !== undefined) return cache[key]
+        const value = fn(args)
+        cache[key] = value
+        return value
+    }
+};
