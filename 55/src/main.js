c592ce9ed07d56a49a954a2721e8f2962947dcd2
BrianPritzel <148987359+BrianPritzel@users.noreply.github.com>
2023-11-03 09:36:27 -0500

    Added leetcode problem 55

---
+++ 55/src/main.js
@@ -0,0 +1,9 @@
+export function canJump(nums) {
+    let goal = nums.length-1
+        for(let i=goal-1; i>=0; i--){
+            if(nums[i]+i >= goal){
+                goal = i
+            }
+        }
+        return goal === 0
+};
